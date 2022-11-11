21ad5b70ced610448922865684c6037eca24cf48
Josip Vučica <jv.vucica@gmail.com>
2022-11-11 22:57:31 +0100

    added fake data, implemented local storage for favourites as a test demo

--- src/hooksTesting/AppR.js
+++ src/hooksTesting/AppR.js
@@ -1,8 +1,10 @@
-import { CircularProgress } from "@mui/material";
+import { Button, CircularProgress } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import RANDOM_URL from "../env/consts";
+import randomJoke from "./fakedata";
+import categories from "./fakecategories";
 
 //This is for testing purposes of hooks, it should be refactored into existing mui implementation
 
@@ -14,32 +16,63 @@ const Spinner = () => {
     );
 }
 
-async function getRandom() {
+function Like(key, value) {
+    useEffect(() => {
+        localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+}
 
-    //TODO: bad request error
-     return axios.get(RANDOM_URL).then(res => {
-         console.log("Got random: " + toString(res));
-     }).catch((err) => console.error(":( Axios error ---> " + err));
+function getStorageValue() {
+    const saved = localStorage.getItem("oo");
+    const initial = JSON.parse(saved);
+    return initial;
+}
+
+/*async*/ function getRandom() {
+
+//dont forget to switch loading to false on end
+
+    //TODO: axios bad request error
+    //  return axios.get(RANDOM_URL).then(res => {
+    //      console.log("Got random: " + toString(res));
+    //  }).catch((err) => console.error(":( Axios error ---> " + err));
 
     // const requestOptions = {
     //     method: "GET",
     // };
-
     // return await fetch(RANDOM_URL, requestOptions).then((res) => {console.log("Res " + toString(res))}).catch((err) => console.error(err));
+
+    return randomJoke;
 }
+
+// export const useLocalStorage = (key, value) => {
+//     const [key, value] = useState(() => {
+//         return getStorageValue(key, value);
+//     }, [key, value]);
+// }
 
 const AppR = () => {
     
     const [loading, setLoading] = useState(false);
+    const [jokes, setJokes] = useState([]);
+    const [liked, setLiked] = useState(() => {
+        return getStorageValue();
+    });
 
     useEffect(() => {
         setLoading(true);
-        getRandom();
+        console.log("Hard coded joke: " + JSON.stringify(randomJoke));
+        setJokes(getRandom());
     }, []);
 
     return(
         <React.Fragment>
         {loading && <Spinner />}
+        <div>
+            {JSON.stringify(jokes)}
+            {toString(categories)}
+        </div>
+        <Button onClick={Like("oo", JSON.stringify(jokes))}>Like</Button>
         </React.Fragment>
     );
 }
---
+++ src/hooksTesting/fakedata.js
@@ -0,0 +1,12 @@
+const randomJoke = {"categories":[],
+    "created_at":"2020-01-05 13:42:23.240175",
+    "icon_url":"https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+    "id":"Jm3r9vk5SISulZ5UQ2lpEw",
+    "updated_at":"2020-01-05 13:42:23.240175",
+    "url":"https://api.chucknorris.io/jokes/Jm3r9vk5SISulZ5UQ2lpEw",
+    "value":"If Chuck Norris ever died... Wait... What am I saying? He can't."
+};
+
+const categories = ["animal","career","celebrity","dev","explicit","fashion","food","history","money","movie","music","political","religion","science","sport","travel"];
+
+export default [randomJoke, categories];
